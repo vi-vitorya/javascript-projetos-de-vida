3a09fc13e7adb011a3898c3a7ed2f3594e8a81b8
vi-vitorya <146095131+vi-vitorya@users.noreply.github.com>
2024-04-18 09:31:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+// Iterando sobre a lista de botões
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
 
-//Iterando sobre a lista de botões
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0; j< botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-        }
-      botoes[i].classList.add("ativo");
-    };
-    console.log()
+//document.querySelector("#minhaDiv p").textContent = "Olá Chuva, venha +- com força";
+const contadores = document.querySelectorAll(".contador");
+const tempoAtual = new Date(); // Retorna a data atual do computador
+const tempoObjetivo1 = new Date("2024-05-02T00:00:00");
+const tempoObjetivo2 = new Date("2024-08-12T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-20T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo) {
+  let tempoFinal = tempoObjetivo - tempoAtual;
+
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
+
+for (let i = 0; i < contadores.length; i++) {
+  contadores[i].textContent = calculaTempo(tempos[i]);
 }
